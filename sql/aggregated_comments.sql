8d8ecc85ed33f9d89b316414553ca47a4265211e
Murilo Cunha <murilo@dataroots.io>
2022-07-28 09:57:49 +0200

    add sql for aggregated values for comments table

---
+++ sql/aggregated_comments.sql
@@ -0,0 +1,41 @@
+WITH dims AS (
+    SELECT
+        record_id,
+        author,
+        link_title,
+        body,
+        subreddit_type,
+        stickied,
+        controversiality,
+        CASE
+            WHEN subreddit = 'AskReddit' THEN 'AskReddit'
+            WHEN subreddit = 'nba' THEN 'nba'
+            WHEN subreddit = 'interestingasfuck' THEN 'interestingasfuck'
+            WHEN subreddit = 'Unexpected' THEN 'Unexpected'
+            ELSE 'Other'
+        END AS subreddit,
+        date(created_utc) AS created_date
+    FROM
+        flattened_comments_typed
+)
+
+SELECT
+    subreddit_type,
+    stickied,
+    controversiality,
+    subreddit,
+    created_date,
+    count(record_id) AS number_or_records,
+    count(DISTINCT author) AS number_of_authors,
+    avg(len(link_title)) AS average_title_length,
+    avg(len(body)) AS average_body_length
+FROM
+    dims
+GROUP BY
+    CUBE(
+        subreddit_type,
+        stickied,
+        controversiality,
+        subreddit,
+        created_date
+    );
