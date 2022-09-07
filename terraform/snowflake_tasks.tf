f457bc3f7237b3f71d03c52c5ed3c9790440569d
Murilo Cunha <murilo@dataroots.io>
2022-09-07 21:54:02 +0200

    use the stream name in the run condition for tasks

--- terraform/snowflake_tasks.tf
+++ terraform/snowflake_tasks.tf
@@ -10,7 +10,7 @@ resource "snowflake_task" "get_new_posts" {
   sql_statement      = file("${path.module}/../sql/tasks/get_new_posts.sql")
   session_parameters = {}
 
-  when    = "system$stream_has_data('new_posts')"
+  when    = "system$stream_has_data('${snowflake_stream.new_posts.name}')"
   enabled = false
 }
 
@@ -25,7 +25,7 @@ resource "snowflake_task" "clean_ml_posts" {
   sql_statement = file("${path.module}/../sql/tasks/clean_ml_posts.sql")
 
   after   = "GET_NEW_POSTS"
-  when    = "system$stream_has_data('new_ml_posts')"
+  when    = "system$stream_has_data('${snowflake_stream.new_ml_posts.name}')"
   enabled = true
 }
 resource "snowflake_task" "predict_ml_posts" {
@@ -39,7 +39,7 @@ resource "snowflake_task" "predict_ml_posts" {
   sql_statement = file("${path.module}/../sql/tasks/predict_ml_posts.sql")
 
   after   = "CLEAN_ML_POSTS"
-  when    = "system$stream_has_data('new_ml_posts_clean')"
+  when    = "system$stream_has_data('${snowflake_stream.new_ml_posts_clean.name}')"
   enabled = true
 }
 
--- terraform/stages.tf
+++ terraform/stages.tf
@@ -2,3 +2,5 @@ data "snowflake_functions" "reddit" {
   database = snowflake_database.snowflake_ml.name
   schema   = snowflake_schema.reddit.name
 }
+
+# TODO: add stages + grants
