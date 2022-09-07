2d351e594cd0fa0a71d202320f54cd62f2262daf
Murilo Cunha <murilo@dataroots.io>
2022-09-07 19:52:19 +0200

    add tasks in terraform

---
+++ sql/tasks/predict_ml_posts.sql
@@ -0,0 +1,22 @@
+INSERT INTO
+ml_posts_predictions(
+    record_id,
+    created_timestamp,
+    subreddit,
+    author,
+    title,
+    is_toxic,
+    model_version
+)
+SELECT
+    record_id,
+    created_timestamp,
+    subreddit,
+    author,
+    title,
+    ML_PREDICT_PROD(title)['prediction'] AS is_toxic,
+    ML_PREDICT_PROD(title)['model_version'] AS model_version
+FROM
+    new_ml_posts_clean
+WHERE
+    metadata$action = 'INSERT'  -- noqa: L057
---
+++ terraform/snowflake_tasks.tf
@@ -0,0 +1,44 @@
+resource "snowflake_task" "get_new_posts" {
+  #  comment = "Get new posts from landing tables."
+
+  database  = snowflake_database.snowflake_ml.name
+  schema    = snowflake_schema.reddit.name
+  warehouse = snowflake_warehouse.reddit_xs.name
+
+  name               = "GET_NEW_POSTS"
+  schedule           = "USING CRON */5 * * * 1-5 Europe/Brussels"
+  sql_statement      = file("${path.module}/../sql/tasks/get_new_posts.sql")
+  session_parameters = {}
+
+  when    = "system$stream_has_data('new_posts')"
+  enabled = false
+}
+
+resource "snowflake_task" "clean_ml_posts" {
+  #  comment = "Get new posts from landing tables."
+
+  database  = snowflake_database.snowflake_ml.name
+  schema    = snowflake_schema.reddit.name
+  warehouse = snowflake_warehouse.reddit_xs.name
+
+  name          = "CLEAN_ML_POSTS"
+  sql_statement = file("${path.module}/../sql/tasks/clean_ml_posts.sql")
+
+  after   = "GET_NEW_POSTS"
+  when    = "system$stream_has_data('new_ml_posts')"
+  enabled = true
+}
+resource "snowflake_task" "predict_ml_posts" {
+  #  comment = "Get new posts from landing tables."
+
+  database  = snowflake_database.snowflake_ml.name
+  schema    = snowflake_schema.reddit.name
+  warehouse = snowflake_warehouse.reddit_xs.name
+
+  name          = "PREDICT_ML_POSTS"
+  sql_statement = file("${path.module}/../sql/tasks/predict_ml_posts.sql")
+
+  after   = "CLEAN_ML_POSTS"
+  when    = "system$stream_has_data('new_ml_posts_clean')"
+  enabled = true
+}
