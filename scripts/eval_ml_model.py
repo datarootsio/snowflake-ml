4dda5db1641917c5ce038cdc28f970bd4a1fb900
Murilo Cunha <murilo@dataroots.io>
2022-09-14 20:30:45 +0200

    predict on the entire test dataset

--- scripts/eval_ml_model.py
+++ scripts/eval_ml_model.py
@@ -84,15 +84,11 @@ def get_classification_report(
 
 def get_table(table: str, session: SnowparkSession) -> DataFrame:
     """Get table from Snowflake."""
-    return (
-        session.table(table)
-        .select(
-            col('"is_toxic"'),
-            call_udf("ml_predict_dev", col('"comment_text"'))["prediction"]
-            .cast(DecimalType())
-            .as_("predicted"),
-        )
-        .limit(1000)
+    return session.table(table).select(
+        col('"is_toxic"'),
+        call_udf("ml_predict_dev", col('"comment_text"'))["prediction"]
+        .cast(DecimalType())
+        .as_("predicted"),
     )
 
 
